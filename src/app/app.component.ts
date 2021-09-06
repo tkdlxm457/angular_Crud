0571aa16d692c56315c14658e295bcef19491330
tkdlxm457 <tkdlxm457@naver.com>
2021-09-06 23:22:19 +0900

    커밋내용을 입력합니다..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { FormGroup, FormControl } from '@angular/forms';
 
 interface Topic {
@@ -21,6 +21,7 @@ export class AppComponent {
     title: new FormControl(''),
     description: new FormControl(''),
   });
+  
   topicList: Topic[] = [];
 
   constructor(private http: HttpClient) {
@@ -34,118 +35,74 @@ export class AppComponent {
         this.topicList = response;
       })
   }
-  
+
   save(): void {
-      this.http
+    this.http
       .post("http://localhost:3000/board",
-      {
-        'title': this.topicForm.controls.title.value,
-        'desc': this.topicForm.controls.description.value
-      },
-      {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-        
+        {
+          'title': this.topicForm.controls.title.value,
+          'desc': this.topicForm.controls.description.value
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+      .subscribe(() => {
+        this.getTopics();
       })
-      .subscribe((response: any) => {
-
-      })
-    // http POST
-
     // subscribe성공콜백에서 재조회
     // this.getTopics();
   }
 
-  selected(topic: Topic):void {
-    console.log(topic);
+  update(): void {
+    console.log("dsadas")
+    this.http
+      .put("http://localhost:3000/board",
+        {
+          'title': this.topicForm.controls.title.value,
+          'desc': this.topicForm.controls.description.value,
+          'id': this.topicForm.controls.id.value,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+      .subscribe((response: any) => {
+        this.getTopics();
+      })
+  }
+
+  delete(): void {
+    console.log("ccccc")
+
+    let headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/json');
+
+    let params = new HttpParams().
+    set('boardId', this.topicForm.controls.id.value).
+    set('xxx', 'safdasdf');
+
     
+    this.http
+      .delete(`http://localhost:3000/board/${this.topicForm.controls.id.value}`,
+        {
+          headers,
+          params
+        }
+      )
+      .subscribe(res => {
+        this.getTopics();
+      })
+  }
+
+
+
+
+  selected(topic: Topic): void {
     this.topicForm.controls.title.setValue(topic.title);
     this.topicForm.controls.description.setValue(topic.description);
     this.topicForm.controls.id.setValue(topic.id);
-
-    console.log(this.topicForm.controls.id.value);
   }
 }
-
-
-// import { Component } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-
-// interface Topic {
-//   id?: number,
-//   title: string,
-//   description: string,
-//   created?: string,
-//   authorId?: number
-// }
-
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: 'app.component.html',
-//   styleUrls: ['app.component.css']
-// })
-// export class AppComponent {
-//   topicForm = new FormGroup({
-//     title: new FormControl(''),
-//     description: new FormControl(''),
-//   });
-
-//   topicList: Topic[] = [];
-
-//   constructor(private http: HttpClient) {
-//     // this.getTopics();
-//     // http요청 보낸 데이터를 datas에다가 넣어주면 끝!
-//   }
-
-//   getTopics() {
-//     this.http.get("http://localhost:3000/board")
-//       .subscribe((response: any) => {
-//         this.topicList = response;
-//       })
-//   }
-
-//   save(): void {
-//     this.topicForm = 
-//     dlqfurg입력한 값이 들어오는지, 들어오면 쏘기
-
-//     this.http.post("http://localhost:3000/board",
-//       {
-//         "title": "",
-//         "description": ""
-//       })
-//       .subscribe((response: any) => {
-
-//       })
-//     // http POST
-
-//     // subscribe성공콜백에서 재조회
-//     // this.getTopics();
-//   }
-
-
-// }
-
-
-
-class 집 {
-  handPhone = ''
-  noteBook = '';
-
-  constructor(handPhone: string, notebook: string, ) {
-    this.handPhone = handPhone;
-    this.noteBook = notebook;
-  }
-
-  notebookSystemOn() {
-    
-  }
-}
-
-function a(xxx: string, bbb: string) {
-
-}
-var home = new 집('asus', '01023720531');
-home.handPhone
-
